00bd32f16fea42a0cdadedcd616c3b25d12b2ce2
Egor <DronovEgorVl@yandex.com>
2021-10-17 14:02:50 +0300

    Created

---
+++ src/main/java/alpha/gifset/controllers/MainController.java
@@ -0,0 +1,14 @@
+package alpha.gifset.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Курс обмена валют");
+        return "index";
+    }
+}
---
+++ src/test/java/alpha/gifset/GifSetApplicationTests.java
@@ -0,0 +1,13 @@
+package alpha.gifset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
