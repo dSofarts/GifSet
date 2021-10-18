67b52a711aab0c51fb5590c0b122a2f50b72d518
Egor <DronovEgorVl@yandex.com>
2021-10-18 15:50:11 +0300

    Creating a currency withdrawal

--- src/main/java/alpha/gifset/controllers/MainController.java
+++ src/main/java/alpha/gifset/controllers/MainController.java
@@ -1,20 +1,45 @@
 package alpha.gifset.controllers;
 
-
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static alpha.gifset.model.MainModel.getDateYesterday;
+import static alpha.gifset.model.MainModel.getUrlContent;
+
 @Controller
 public class MainController {
 
-    public String currencyName;
+    @Value("${openexchangerates.url.general}")
+    private String url;
+    @Value("${openexchangerates.app.id}")
+    private String api;
+
     public double currency;
+    public double newCurr;
+    public String pastCurr;
 
     @GetMapping("/")
     public String home(Model model) {
+        String nowOutput = getUrlContent( url + "/latest.json?app_id=" + api);
+        String pastOutput = getUrlContent(url + "/historical/" + getDateYesterday() + ".json?app_id=" + api);
+        if (!nowOutput.isEmpty() && !pastOutput.isEmpty()) {
+            JSONObject nowObj = new JSONObject(nowOutput);
+            JSONObject pastObj = new JSONObject(pastOutput);
+            newCurr = nowObj.getJSONObject("rates").getDouble("RUB");
+            double pastCurrDouble = Math.round((newCurr - pastObj.getJSONObject("rates").getDouble("RUB")) * 100) / 100.00;
+            if (pastCurrDouble > 0) {
+                pastCurr = "+ " + Double.toString(pastCurrDouble);
+            } else {
+                pastCurr = Double.toString(pastCurrDouble);
+            }
+        }
         model.addAttribute("title", "Курс обмена валют");
         model.addAttribute("currency", currency);
+        model.addAttribute("newCurr", newCurr);
+        model.addAttribute("pastCurr", pastCurr);
         return "index";
     }
 }
---
+++ src/main/java/alpha/gifset/model/MainModel.java
@@ -0,0 +1,34 @@
+package alpha.gifset.model;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainModel {
+    public static String getUrlContent(String urlAddress) {
+        StringBuffer content = new StringBuffer();
+        try {
+            URL url = new URL(urlAddress);
+            URLConnection urlConnection = url.openConnection();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return content.toString();
+    }
+    public static String getDateYesterday() {
+        Calendar date = Calendar.getInstance();
+        date.add(Calendar.DATE, -1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date.getTime());
+    }
+}
